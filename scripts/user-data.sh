b6308840ac76cab958896b5e093b7aa77cef71c5
adebande <63793809+adebande@users.noreply.github.com>
2021-11-29 14:31:11 +0100

    add apache service start cmd

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
 while ! ping -c 1 -W 1 8.8.8.8; do
     echo "Waiting for internet connection"
     sleep 1
 done
-
 sudo apt-get update
-
 sudo apt-get -y install apache2
-
 sudo service apache2 start
